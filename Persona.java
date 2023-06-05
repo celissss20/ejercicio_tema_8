2d9c4b706960caa00aec79ddc20096e908d33dfc
Celis <dedsa2016@gmail.com>
2023-06-05 13:34:29 -0500

    resolviendo ejercicio tema 8

---
+++ Persona.java
@@ -0,0 +1,43 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        //setters para asignar valores 
+        persona.setEdad(22);
+        persona.setNombre("Daniel");
+        persona.setTelefono("3216549870");
+
+        //getters para obtener los valores y mostrarlos por consola
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Teléfono: " + persona.getTelefono());
+    }
+}
